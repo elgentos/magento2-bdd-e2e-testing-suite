d1b520d2d97aead3a9c442c4a4e473f29cb6850a
Shay <shayfaber@gmail.com>
2024-12-09 14:54:43 +0100

    Add test to remove product from Cart page

--- tests/refactor/fixtures/cart.page.ts
+++ tests/refactor/fixtures/cart.page.ts
@@ -1,5 +1,5 @@
 import {expect, type Locator, type Page} from '@playwright/test';
-
+import selectors from '../config/selectors/selectors.json';
 
 export class CartPage {
   readonly page: Page;
@@ -8,5 +8,13 @@ export class CartPage {
   constructor(page: Page) {
     this.page = page;
     this.applyDiscountButton = this.page.getByRole('button', { name: 'Apply Discount Code' });
+  }
+
+  async removeProduct(name: string){
+    console.log(name);
+    let removeButton = this.page.getByLabel(`Remove ${name}`);
+    await removeButton.click();
+
+    await expect(removeButton,`Button to remove product is no longer visible`).toBeHidden();
   }
 }
