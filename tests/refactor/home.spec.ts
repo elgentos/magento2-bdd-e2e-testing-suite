86e1af2034650259fa95ea47e6a36d94d1fd4c16
Shay <shayfaber@gmail.com>
2024-12-09 14:28:07 +0100

    Improvement on description of TODO in home.spec.ts

--- tests/refactor/home.spec.ts
+++ tests/refactor/home.spec.ts
@@ -9,7 +9,7 @@ test('Add product on homepage to cart',{ tag: '@homepage',}, async ({page}) => {
   const homepage = new HomePage(page);
   const mainmenu = new MainMenuPage(page);
 
-  // TODO: rework into API call to reduce test time
+  // TODO: rework adding product to cart into API call function to reduce test time
   await page.goto('');
   await homepage.addHomepageProductToCart();
   await mainmenu.openMiniCart();
